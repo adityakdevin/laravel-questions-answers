c8d469c4fb0113834d92957c0e06d5c953ce63e4
Aditya Fullstackdeveloper <adityafullstackdeveloper@gmail.com>
2019-09-23 15:28:13 +0530

    Dispaly Answer for  Question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -4,60 +4,53 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h2>{{ $question->title }}</h2>
-                            <div class="ml-auto">
-                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all Ask Question</a>
-                            </div>
-                        </div>
-                    </div>
                     <div class="card-body">
-                        {!! $question->body_html !!}
-                        <div class="float-right">
-                            <span class="text-muted">{{ $question->create_date }}</span>
-                            <div class="media mt-2">
-                                <a href="{{ $question->user->url }}" class="pr-2">
-                                    <img src="{{ $question->user->avatar }}" alt="{{ $question->user->name }}">
-                                </a>
-                                <div class="media-body mt-1">
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                        <div class="cart-title">
+                            <div class="d-flex align-items-center">
+                                <h2>{{ $question->title }}</h2>
+                                <div class="ml-auto">
+                                    <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all Ask Question</a>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-title">
-                            <h2>{{ $question->answers_count." ". Str::plural('Answer',$question->answers_count) }}</h2>
-                        </div>
-                        <hr/>
-                        @foreach($question->answers as $answer)
-                            <div class="media">
-                                <div class="media-body">
-                                    {!! $answer->body_html !!}
-                                    <div class="float-right">
-                                        <span class="text-muted">Answered {{ $answer->create_date }}</span>
-                                        <div class="media mt-2">
-                                            <a href="{{ $answer->user->url }}" class="pr-2">
-                                                <img src="{{ $answer->user->avatar }}" alt="{{ $answer->user->name }}">
-                                            </a>
-                                            <div class="media-body mt-1">
-                                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                            </div>
+                        <hr>
+                        <div class="media">
+                            <div class="d-fex flex-column vote-controls">
+                                <a title="This question is useful" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1230</span>
+                                <a title="This question is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                    <i class="fas fa-star fa-2x"></i>
+                                    <span class="favorites-count">123</span>
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                {!! $question->body_html !!}
+                                <div class="float-right">
+                                    <span class="text-muted">Answered {{ $question->created_date }}</span>
+                                    <div class="media mt-2">
+                                        <a href="{{ $question->user->url }}" class="pr-2">
+                                            <img src="{{ $question->user->avatar }}">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        @include('answers._index',[
+        'answersCount'=>$question->answers_count,
+        'answers'=>$question->answers
+        ])
+        @include('answers._create')
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@
     
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource("questions", "QuestionsController")->except('show');
+    //Route::post("/questions/{question}/answers", "AnswersController")->name('answers.store');
+    Route::resource("questions.answers", "AnswersController")->except(['index','create','show']);
     Route::get("/questions/{slug}", "QuestionsController@show")->name('questions.show');
